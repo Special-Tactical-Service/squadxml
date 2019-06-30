cc98214cc12bd628c96618164b355eba113b968d
Marvin Blum <marvin@marvinblum.de>
2019-06-30 22:52:24 +0200

    Database connection and xml generation.

--- Dockerfile
+++ Dockerfile
@@ -9,10 +9,16 @@ RUN go build -ldflags "-s -w" main.go
 FROM alpine
 COPY --from=build /go/src/github.com/Kugelschieber/squadxml /app
 WORKDIR /app
+RUN mkdir /squadxml
+RUN chmod +x /app/main
 
 # default config
 ENV SQUADXML_PATH=/squadxml
 ENV SQUADXML_HOST=0.0.0.0:80
+ENV SQUADXML_DB_USER=user
+ENV SQUADXML_DB_PASSWORD=password
+ENV SQUADXML_DB_HOST=host
+ENV SQUADXML_DB=db
 
 VOLUME ["/squadxml"]
 CMD ["/app/main"]
--- main.go
+++ main.go
@@ -1,19 +1,109 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 const (
-	squadxmlFile            = "squad.xml"
-	defaultHttpWriteTimeout = 20
-	defaultHttpReadTimeout  = 20
+	squadxmlFile = "squad.xml"
+	squadxmlLogo = "logo.paa"
+	squadxmlHead = `<?xml version="1.0"?>
+		<!DOCTYPE squad SYSTEM "squad.dtd">
+		<?xml-stylesheet href="squad.xsl" type="text/xsl"?>`
+	squadxmlSquad = `<squad nick="sTs">
+		<name>Special Tactical Service</name>
+		<email>noreply@sts.wtf</email>
+		<web>www.sts.wtf</web>
+		<picture>` + squadxmlLogo + `</picture>
+		<title>Special Tactical Service</title>`
+	squadxmlEnd = `</squad>`
 )
 
+type Member struct {
+	UserOption32 string `db:"userOption32"`
+	Username     string `db:"username"`
+	RankTitle    string `db:"rankTitle"`
+	UserOption33 string `db:"userOption33"`
+}
+
+var (
+	db *sqlx.DB
+)
+
+func connectToDB() {
+	user := os.Getenv("SQUADXML_DB_USER")
+	password := os.Getenv("SQUADXML_DB_PASSWORD")
+	host := os.Getenv("SQUADXML_DB_HOST")
+	database := os.Getenv("SQUADXML_DB")
+	var err error
+	db, err = sqlx.Connect("mysql", mysqlConnection(user, password, host, database))
+
+	if err != nil {
+		logrus.WithError(err).Fatal("Error connecting to database")
+	}
+
+	if err := db.Ping(); err != nil {
+		logrus.WithError(err).Fatal("Error pinging database")
+	}
+}
+
+func mysqlConnection(user, password, host, database string) string {
+	return user + ":" + password + "@" + host + "/" + database
+}
+
+func buildSquadXML() {
+	for {
+		time.Sleep(time.Minute * 10)
+		member := getMember()
+
+		if member != nil {
+			writeSquadXMLToFile(member)
+		}
+	}
+}
+
+func getMember() []Member {
+	query := `SELECT wcf1_user_option_value.userOption32,
+		wcf1_user.username,
+		wcf1_user_rank.rankTitle,
+		wcf1_user_option_value.userOption33
+        FROM wcf1_user, wcf1_user_option_value, wcf1_user_to_group, wcf1_user_rank
+        WHERE wcf1_user.userID = wcf1_user_to_group.userID AND wcf1_user.userID = wcf1_user_option_value.userID AND wcf1_user_to_group.groupID = 6 AND wcf1_user.rankID = wcf1_user_rank.rankID AND wcf1_user_option_value.userOption32 != 0
+        ORDER BY wcf1_user.userID ASC`
+	var member []Member
+
+	if err := db.Select(member, query); err != nil {
+		logrus.WithError(err).Error("Error selecting members")
+		return nil
+	}
+
+	return member
+}
+
+func writeSquadXMLToFile(member []Member) {
+	xml := squadxmlHead + squadxmlSquad
+
+	for _, m := range member {
+		xml += `<member id="` + m.UserOption32 + `" nick="` + m.Username + `">
+			<name>` + m.Username + `</name>
+			<email></email>
+			<icq>N/A</icq>
+			<remark>` + m.RankTitle + ` - ` + m.UserOption33 + `</remark>
+			</member>`
+	}
+
+	xml += squadxmlEnd
+}
+
 func main() {
+	connectToDB()
+	go buildSquadXML()
 	path := os.Getenv("SQUADXML_PATH")
 	logrus.WithField("squadxml_path", path).Info("Starting server...")
 
